04503f0797bfac3ae90ee8c1485b1917653e209b
drunksu <1073344560@qq.com>
2024-12-19 23:25:33 +0800

    选择排序
    在未排序的区间找到最小值，并把它放到已排序的末尾。
    时间复杂度为O(n²）  空间复杂度为O（1）。

---
+++ 排序/selection_sort.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+void sort(int *nums,int n){
+	for(int i = 0;i < n;i++){
+		int temp = nums[i];
+		int index = i;
+		for(int j = i;j < n;j++){
+			if(nums[j] < temp){
+				temp = nums[j];
+				index = j;
+			}//to find the minimum number of the remaining part
+		}
+		nums[index] = nums[i];
+		nums[i] = temp;
+	}
+}
+int match(int *a,int *b){
+	int n = sizeof(a),m = sizeof(b);
+	if(n != m){
+		return 0;
+	}
+	for(int i = 0;i < n;i++){
+		if(a[i] != b[i]){
+			return 0;
+		}
+	}
+	return 1;
+}
+
+void Testcase1(){
+	int nums[] = {2,5,6,1,4,3};
+	sort(nums,6);
+	int ans[] = {1,2,3,4,5,6};
+	assert(match(nums,ans) == 1);
+	printf("Pass Testcase1!\n");
+}
+
+void Testcase2(){
+	int max = 128;
+	int *nums = (int*)malloc(max*sizeof(int));
+	int *ans = (int*)malloc(max*sizeof(int));
+	for(int i = 0;i < max;i++){
+		nums[i] = max / 2 - i;
+	}
+	sort(nums,max);
+	for(int i = 0;i < max;i++){
+		ans[i] = 1 - max / 2 + i;	
+	}
+	assert(match(nums,ans));
+	printf("Pass Testcase2!\n");
+	
+}
+
+int main(){
+	Testcase1();
+	Testcase2();
+	
+	return 0;
+}
