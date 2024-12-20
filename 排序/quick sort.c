25fdabad730a7c573dbfeed18b6eac9279ee2124
drunksu <1073344560@qq.com>
2024-12-20 12:09:32 +0800

    Update quick sort.c

--- 排序/quick sort.c
+++ 排序/quick sort.c
@@ -38,6 +38,44 @@ void sort(int nums[], int left, int right) {
 	sort(nums, pivot + 1, right);
 }
 
+void quicksort(int arr[], int start, int end)
+{
+	if (start >= end)
+		return;
+	int i = start;
+	int j = end;
+	// 基准数
+	int baseval = arr[start];
+	while (i < j)
+	{
+		// 从右向左找比基准数小的数
+		while (i < j && arr[j] >= baseval)
+		{
+			j--;
+		}
+		if (i < j)
+		{
+			arr[i] = arr[j];
+			i++;
+		}
+		// 从左向右找比基准数大的数
+		while (i < j && arr[i] < baseval)
+		{
+			i++;
+		}
+		if (i < j)
+		{
+			arr[j] = arr[i];
+			j--;
+		}
+	}
+	// 把基准数放到i的位置
+	arr[i] = baseval;
+	// 递归
+	quicksort(arr, start, i - 1);
+	quicksort(arr, i + 1, end);
+}
+
 
 int match(int *a,int *b){
 	int n = sizeof(a),m = sizeof(b);
